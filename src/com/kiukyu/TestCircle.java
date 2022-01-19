1123aa1b2d885107a1e123ac04053c49dbd8d850
Kiu-Kyu <minkiukiu@gmail.com>
2022-01-20 00:24:20 +0700

    add 2 files

---
+++ src/com/kiukyu/TestCircle.java
@@ -0,0 +1,14 @@
+package com.kiukyu;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle();
+        Circle circle2 = new Circle(5);
+
+        System.out.println(circle1.getRadius());
+        System.out.println(circle1.getArea());
+
+        System.out.println(circle2.getRadius());
+        System.out.println(circle2.getArea());
+    }
+}
